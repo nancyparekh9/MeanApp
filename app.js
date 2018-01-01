abaad38caba44be1ec0b9950c0a781a57047c7e9
Nancy <nancyparekh9@gmail.com>
2018-01-02 01:10:40 +0530

    mm

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require ('express');
+const path = require ('path');
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
+const passport = require ('passport');
+const mongoose = require ('mongoose');
+
+const config = require('./config/database');
+
+mongoose.connect();
+
+const app = express();
+
+const port = 3000;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const users = require('./routes/users');
+app.use('/users',users);
+
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.send('Home1');
+});
+
+
+app.listen(port, () => console.log(port));
+
