fbfc5f78bf2b376af5cadac4a0ab9c66a13e2935
Nancy <nancyparekh9@gmail.com>
2018-01-02 02:08:01 +0530

    mm

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,47 @@
-<<<<<<< HEAD
+
+const express = require ('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/user');
+
+router.post('/register',(req,res,next)=>{
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err,user) =>{
+        if(err){
+            res.json({success: false , msg:'Failed to register user'});
+        }
+        else{
+            res.json({success: true , msg:'User registered'});
+        }
+    });
+
+
+});
+router.post('/authentication',(req,res,next)=>{
+    res.send('Authenticate');
+
+
+});
+
+router.get('/profile',(req,res,next)=>{
+    res.send('PROFILE');
+
+
+});
+
+
+
+
+
+
+
 const express = require ('express');
 
 const router = express.Router();
@@ -27,34 +70,5 @@ router.get('/validate',(req,res,next)=>{
 
 
 
-=======
-const express = require ('express');
 
-const router = express.Router();
-
-router.get('/register',(req,res,next)=>{
-    res.send('REGISTER');
-
-
-});
-router.get('/authentication',(req,res,next)=>{
-    res.send('Authenticate');
-
-
-});
-
-router.get('/profile',(req,res,next)=>{
-    res.send('PROFILE');
-
-
-});
-router.get('/validate',(req,res,next)=>{
-    res.send('VALIDATE');
-
-
-});
-
-
-
->>>>>>> 385e1148437bd27592c48968350f1eb6a4fac4a8
 module.exports = router;
